dc5c72d9d6965988afb8375ae512d96c177708f7
zshafique <121639533+zshafique@users.noreply.github.com>
2023-08-09 19:28:52 +0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ release = '24 July 2023'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [ 'sphinx_simplepdf', 'sphinx_simplepdf']
+extensions = [ 'sphinx-simplepdf']
 
 templates_path = ['_templates']
 exclude_patterns = []
